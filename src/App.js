9399676f5911b204b34d66de16e7a9ccb1ce86c7
amargope <amargope05@gmail.com>
2022-06-02 16:35:09 +0530

    1 file modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./style.css";
 import Search from './components/Search'
-import Home from './components/Home'
+
 
 export default function App() {
   return (
     <div>
-     {/* <Search/> */}
-     <Home />
+     <Search/>
+   
     </div>
   );
 }
